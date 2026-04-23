dde8353bf947f1c7ccedc612eb44a8a6b6af1bfd
Rohit Nehra <rohitnehra483@gmail.com>
2026-04-23 12:00:05 +0530

    successfully uploaded

---
+++ UI.py
@@ -0,0 +1,15 @@
+import streamlit as  st
+
+st.title("Real-time-price-monitoring-Alert-System")
+st.header("Daily Price  Traking")
+st.header("Daily Price  Storage")
+st.header("Daily Price  Comparison")
+st.header("Alert  System for Price  Change")
+st.header("Autamated pipline")
+
+st.markdown("### Database connective")
+   
+st.success("Proceed Successfully")
+st.info("Find the Formal information")
+st.warning("You are going to high risk scenario")
+st.error("Accunt is Bloced for 24 hours")   
